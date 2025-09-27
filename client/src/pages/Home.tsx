eb0d740e066a6e92e2070eafef990c3cd1f63189
yaman-opse <shagareenyaman@gmail.com>
2025-09-27 19:55:39 -0400

    Upload client/src/pages/Home.tsx

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import TypingGame from '@/components/TypingGame';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background py-8">
+      <TypingGame />
+    </div>
+  );
+}
